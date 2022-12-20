0865d49ed10a3af3e506149b55ba9405c83e9e1e
30Ashish <30ashishofficial@gmail.com>
2022-12-20 23:59:36 +0530

    bootsatrap_dynamic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import { useState } from "react";
+
 function App() {
+  let [theme, setTheme] = useState("warning");
+
+  let makePrimaryTheme = () => {
+    theme = "primary";
+    setTheme(theme);
+  };
+  let makeSuccessTheme = () => {
+    theme = "success";
+    setTheme(theme);
+  };
+  let makeDangerTheme = () => {
+    theme = "danger";
+    setTheme(theme);
+  };
   return (
     <div>
-      <h1 className="bg-warning text-light p-3">Bootstrap</h1>
+      <h1 className={`bg-${theme} text-light p-3`}>Theme light{theme}</h1>
 
-      <h1 className="bg-info" style={{ color: "#fff" }}>
-        Ashish
-      </h1>
-      <h1 className="bg-danger" style={{ color: "#fff" }}>
-        CDAC
-      </h1>
+      <input
+        className="btn btn-primary"
+        type="button"
+        value="Primary"
+        onClick={makePrimaryTheme}
+      />
+      <input
+        className="btn btn-success"
+        type="button"
+        value="Success"
+        onClick={makeSuccessTheme}
+      />
+      <input
+        className="btn btn-danger"
+        type="button"
+        value="Denger"
+        onClick={makeDangerTheme}
+      />
     </div>
   );
 }
