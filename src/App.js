e5132e19d63faca6147ea9f2ddb1c685bf32c842
30Ashish <30ashishofficial@gmail.com>
2022-12-20 23:46:57 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 function App() {
   return (
     <div>
-      <h1 className="beautify">External Styling with React</h1>
+      <h1 className="bg-warning text-light p-3">Bootstrap</h1>
+
+      <h1 className="bg-info" style={{ color: "#fff" }}>
+        Ashish
+      </h1>
+      <h1 className="bg-danger" style={{ color: "#fff" }}>
+        CDAC
+      </h1>
     </div>
   );
 }
