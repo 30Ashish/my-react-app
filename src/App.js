f14bbaf9a231ac596ecf6ec28d98662d9a638daf
30Ashish <30ashishofficial@gmail.com>
2022-12-20 17:04:38 +0530

    basic-react-styling

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
-// function App() {
-//   return "Hello Ashish";
-// }
+import "./App.css";
 
-// export default App;
-
-//b1 - jsx - single line
-// function App() {
-//   return <h1>Ashish Londhe</h1>
-// }
-// export default App;
-
-/* b2 - jsx - multiline*/
-// function App() {
-//   return (<h1>Software developers create the computer applications that allow users to do specific tasks
-//     and the underlying systems that run the devices or control networks.
-//     Software quality assurance analysts and testers design and execute software tests to identify problems and learn how the software works.
-//   </h1>);
-// }
-// export default App;
-
-/* b2- multi tags */
-// function App() {
-//   return (
-//     <div>
-//       <h1>HElloooo Guysss</h1>
-//     </div>
-//   );
-// }
-// export default App;
-
-/* b2-multiple jx*/
 function App() {
+  let mystyle = { color: "white", background: "purple" };
+
   return (
     <div>
-      <Person name="Ashish" />
-      <Person name="Londhe" />
-      <Person name="Sindhudurg" />
-      <Person name="Sawantwadi" />
-      <Person name="HELLO" />
-    </div>
-  );
-}
-function Person({ name }) {
-  return (
-    <div>
-      <img src="https://picsum.photo/300" alt=" " />
-      <h1>Person name :{name}</h1>
-      <p>
-        Software developers create the computer applications that allow users to
-        do specific tasks and the underlying systems that run the devices or
-        control networks. Software quality assurance analysts and testers design
-        and execute software tests to identify problems and learn how the
-        software works. Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Veniam obcaecati libero, maiores corrupti aliquid unde, commodi,
-        soluta omnis sed vel possimus magnam doloribus quidem autem. Delectus
-        tenetur expedita repellendus deleniti.
-      </p>
+      <h1 className="beautify">Hello Ashish</h1>
+
+      {/** This is kind of internal. Note: Only one curly braces. */}
+      <h1 style={mystyle}>Badal ka Diwana kon hai??</h1>
+
+      {/* THIS IS INLINE STYLING */}
+      <h1 style={{ color: "white", backgroundColor: "red" }}>Hello</h1>
     </div>
   );
 }
